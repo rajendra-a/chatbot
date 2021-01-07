c9736075d404f94adab581555ee943216021bb5c
rajendra-a <rajendrareddy.akkala@gmail.com>
2021-01-07 14:07:19 +0530

    updated

--- actions/actions.py
+++ actions/actions.py
@@ -6,22 +6,46 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
+import smtplib, ssl
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+from rasa_sdk.events import SlotSet
+
+
+class ActionSendEmail(FormAction):
+
+    def name(self) -> Text:
+        return "action_send_mail"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        username = tracker.get_slot("username")
+        email = tracker.get_slot("email-address")
+        phone = tracker.get_slot("phone-number")
+
+        smtp_server = syberzen.com
+        port = 465
+        sender_email = "rajendrareddy.akkala@gmail.com"
+        receiver_email = "rajendra@syberzen.com"
+        password = "Rajendra@s11*#"
+
+        message = f"this message is sent from chatbot {username}, {email}, {phone}"    
+
+        # create secure ssl context
+        context = ssl.create_default_context()
+
+        # try to login server
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, receiver_email)
+            server.sendemail(sender_email, receiver_email, message)
+
+        dispatcher.utter_message(text="Soon we will contact you")
+
+        return [Slotset("username", None),
+                SlotSet("email-address", None),
+                SlotSet("phone-number", None)]
